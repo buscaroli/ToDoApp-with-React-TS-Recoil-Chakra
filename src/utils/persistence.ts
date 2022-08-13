597c86ead84db035869ea8e8b33ac024ad38fa01
mabus <m.buscaroli@outlook.com>
2022-08-13 09:07:17 +0100

    improve the persistance module by adding a "reset" case

--- src/utils/persistence.ts
+++ src/utils/persistence.ts
@@ -1,16 +1,22 @@
 import { Todo } from "../models/todo"
 
-export const persist = (action: string, key: string, payload: Todo[] | null) => {
+export const persist = (action: string, key: string, payload: Todo[]) => {
   switch(action) {
     case 'read':
-      const data = JSON.parse(localStorage.get(key)) ?? null
+      const data: string | null = localStorage.getItem(key)
+      
       if (data) {
-        return data
+        return JSON.parse(data)
       } else {
         return []
       }
+      
     case 'write':
-      localStorage.set(key, JSON.stringify(payload))
+      localStorage.setItem(key, JSON.stringify(payload))
+      break
+    
+    case 'reset':
+      localStorage.setItem(key, JSON.stringify([]))
       break
     default:
       throw new Error('Action not recognised while trying to access Local Storage')
